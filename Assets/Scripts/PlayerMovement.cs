5b99b34f1ec4ca5b0b7d006c64aeba1a921d003d
Mohamed <104508524+mjadid91@users.noreply.github.com>
2026-06-01 14:45:20 +0200

    suivi du tuto, creation de myRigidbody dans PlayerMovement, recuperation de la composante rigibody2D dans player, creation et appel de run à chaque update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,25 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Vector2 moveInput;
+    private Rigidbody2D myRigidbody2D;
+
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+    }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+
     }
 }
