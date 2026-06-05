c017528224238629ace56c82f35bae4902765515
mohamed jadid <med.jadid@hotmail.com>
2026-06-05 16:26:47 +0200

    MAJ du script EnemyMovement.cs et ajout de box collider 2d avec trigger, ajout du ontriggerexit2d

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Rigidbody2D myRigidbody2D;
+
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        myRigidbody2D.linearVelocity = new Vector2(
+            moveSpeed,
+            0f
+        );
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector3(
+            -Mathf.Sign(moveSpeed),
+            1f,
+            1f
+        );
+    }
+}
