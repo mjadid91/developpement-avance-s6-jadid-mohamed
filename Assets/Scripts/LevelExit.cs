2b07d699fb07420cc954d988e204ef14ae995faf
Mohamed <104508524+mjadid91@users.noreply.github.com>
2026-06-06 17:40:46 +0200

    finalisation du tutoriel 7, coche du isTrigger pour la porte de transition

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -7,6 +7,11 @@ public class LevelExit : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
         Invoke("LoadNextScene", sceneLoadDelay);
     }
 
